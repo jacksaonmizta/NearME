be90465fb7a7f562060b100637ec580ebb1d7a9e
shuresh96 <57911933+shuresh96@users.noreply.github.com>
2020-01-14 21:26:15 +0800

    update from pc

--- app/src/main/java/com/example/nearme/MainActivity.java
+++ app/src/main/java/com/example/nearme/MainActivity.java
@@ -29,7 +29,7 @@ public class MainActivity extends AppCompatActivity {
                 Intent intent = new Intent(MainActivity.this, Login_Form.class);
                 intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK
                         | Intent.FLAG_ACTIVITY_CLEAR_TOP
-                        | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+                        );
                 startActivity(intent);
                 finish();
 
