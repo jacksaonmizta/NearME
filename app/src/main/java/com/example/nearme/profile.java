b6336812022ce4ab09d8ec42f84f783935665ca9
shuresh96 <57911933+shuresh96@users.noreply.github.com>
2020-02-10 00:42:32 +0800

    deleted ggole maps api

--- app/src/main/java/com/example/nearme/profile.java
+++ app/src/main/java/com/example/nearme/profile.java
@@ -62,6 +62,7 @@ public class profile extends AppCompatActivity {
                 intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK
                         | Intent.FLAG_ACTIVITY_CLEAR_TOP
                         );
+
                 startActivity(intent);
                 finish();
 
