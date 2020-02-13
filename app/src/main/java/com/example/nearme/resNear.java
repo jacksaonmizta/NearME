85989af048f7a62be9a312d4f0deb2052557a080
jacksaonmizta <miztarock26@gmail.com>
2020-02-13 08:40:54 +0800

    upated

---
+++ app/src/main/java/com/example/nearme/resNear.java
@@ -0,0 +1,14 @@
+package com.example.nearme;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class resNear extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_res_near);
+    }
+}
